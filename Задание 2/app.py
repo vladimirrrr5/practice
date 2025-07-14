ca28e262a4f46f84671c7e8d637a83e71f8922c5
Vladimir <vladimirmolchanov1111@gmail.com>
2025-07-14 14:56:42 +0300

    Исправлена ошибка в названии файла

--- Задание 2/app.py
+++ Задание 2/app.py
@@ -1,4 +1,3 @@
-#12314123
 from flask import Flask, jsonify
 import math
 
@@ -10,4 +9,4 @@ def gcd(a, b):
     return jsonify({"number1": a, "number2": b, "gcd": result})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=100)
